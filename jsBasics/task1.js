1a89c5c61dc4c81fa46e60ab8ef6730593b24d5c
irynakozak <iryna.kozak.qa@gmail.com>
2024-01-26 20:11:36 +0100

    homework 3. JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+const phoneNumber = "+380500140060"
+const score = 100
+const hasDiscount = true
+const secondName = null
+const name = undefined
+
+
+console.log("String:", phoneNumber)
+console.log("Number:", score)
+console.log("Boolean:", hasDiscount)
+console.log("Null:", secondName)
+console.log("Undefined:", name)
+
