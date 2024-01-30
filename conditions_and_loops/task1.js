d6f8a3ca03e39d0b46ee6db5371d5d867ab5be88
irynakozak <iryna.kozak.qa@gmail.com>
2024-01-30 14:01:43 +0100

    homework 4. Conditions and loops

---
+++ conditions_and_loops/task1.js
@@ -0,0 +1,17 @@
+const averageGrade = 73
+let successRate
+
+if (averageGrade >= 0 && averageGrade < 60){
+    successRate = "Незадовільно"
+}  else if (averageGrade >= 60 && averageGrade <= 70){
+    successRate = "Задовільно"
+}  else if (averageGrade >= 71 && averageGrade <= 80){
+    successRate = "Добре"
+}  else if (averageGrade >= 81 && averageGrade <= 90){
+    successRate = "Дуже добре"
+}  else if (averageGrade >= 91 && averageGrade <= 100){
+    successRate = "Відмінно"
+}  else {
+    successRate = "Unhandled average grade"
+}
+console.log(successRate)
