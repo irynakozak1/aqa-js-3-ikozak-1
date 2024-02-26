8a72343b44a3ad4d5e514bcaf6af46f99bace3c5
irynakozak <iryna.kozak.qa@gmail.com>
2024-02-26 14:44:04 +0100

    homework 10. JavaScript Promises

---
+++ promises/Todo.js
@@ -0,0 +1,13 @@
+export default class TodoController {
+    constructor(baseURL) {
+        this._baseURL = baseURL
+    }
+    async fetchTodo(id) {
+        const response = await fetch(`${this._baseURL}/todos/${id}`)
+        const body = await response.json()
+        // console.log(body)
+        return body
+    }
+}
+
+
---
+++ promises/task4.js
@@ -0,0 +1,21 @@
+import TodoController from "./Todo.js";
+import UserController from "./User.js";
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+const todoController = new TodoController(BASE_URL)
+const userController = new UserController(BASE_URL)
+
+Promise.all([
+    todoController.fetchTodo(1),
+    userController.fetchUser(1)
+]).then(([todo, user]) => {
+    console.log("Todo:", todo)
+    console.log("User:", user)
+})
+
+Promise.race([
+    todoController.fetchTodo(1),
+    userController.fetchUser(1)
+]).then((result) => {
+    console.log("Race result:", result)
+})
